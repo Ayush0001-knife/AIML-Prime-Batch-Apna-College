02b469573b5cf875f8e5197ec293e0dd9c984e14
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-06 20:00:00 +0530

    Python

--- Python/input-output-operations.py
+++ Python/input-output-operations.py
@@ -1,6 +1,12 @@
-f=open("sample.txt","r")
-complete_data=f.read()
-oneLine=f.readline()
-print(complete_data)
-print(oneLine)
-f.close()
+f=open("file.txt","r")
+f=open("file.txt","w")
+
+data = f.read()
+print(data)
+
+data=f.readline()
+print(data)
+
+f.write("Previous data will be removed.")
+data=f.read()
+print(data)
