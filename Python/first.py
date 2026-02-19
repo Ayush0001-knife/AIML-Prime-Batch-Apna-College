4f35a2c6dcdb20753a1ffa86a94f954fd2eb1d77
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-19 21:46:58 +0530

    Python-Rough-Practice

---
+++ Python/first.py
@@ -0,0 +1,16 @@
+class Laptop:
+      storageType='SSD'
+
+      def __init__(self,ram,storage):
+            self.ram=ram
+            self.storage=storage
+
+      def get_info(self):
+            print(f"Laptop has {self.ram} GB RAM and {self.storage} GB {self.storageType}.")      
+
+
+l1=Laptop(16,512)
+l2=Laptop(8,256)  
+
+l1.get_info()
+l2.get_info()
