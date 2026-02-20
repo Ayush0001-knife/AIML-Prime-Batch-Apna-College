a1e4dabd58046b98200c13f6ba0ac408fd0da8e1
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-20 20:00:00 +0530

    Python

--- Python/json-module-practice2.py
+++ Python/json-module-practice2.py
@@ -1,6 +1,13 @@
 import json
 
+sample_dict={
+      "name":"Kanwal",
+      "school":"Notre Dame"
+}
+
 with open("data.json","r") as f:
       py_obj=json.load(f)
-
       print(py_obj)
+
+with open("data.json","w") as f:
+      json.dump(sample_dict,f)
