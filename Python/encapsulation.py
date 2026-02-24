15f28e66da21b3e5aa56016839bedd2a65143b18
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-24 21:48:46 +0530

    Python

--- Python/encapsulation.py
+++ Python/encapsulation.py
@@ -1,10 +1,16 @@
 class bankAccount:
       def __init__ (self,name,balance,accNo):
-            self.name=name
-            self._balance=balance
-            self.__accNo=accNo
+            self.name=name  # public variable  
+            self._balance=balance   # protected variable (convention to indicate it should not be accessed directly outside the class)
+            self.__accNo=accNo     # private variable (name mangling to prevent direct access)
+
+
+      def get_accNo(self):    # getter function to access private variable
+            return self.__accNo  
+
+      
 
 acc1=bankAccount("Ayush",10000,1234567890)
 print(acc1.name) 
-print(acc1._balance)      
-print(acc1.__accNo)  # This will raise an AttributeError because __accNo is private
+print(acc1._balance)
+print(acc1.get_accNo())  # accessing private variable through getter function
