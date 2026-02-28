b0dd8e90e29a6c65f9b16b69fad7801566a3a82b
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-28 09:56:10 +0530

    Python

--- Python/inheritance.py
+++ Python/inheritance.py
@@ -15,10 +15,16 @@ class AdminStaff(Employee):
             self.name=name
             self.department=department
 
+class Accountant(AdminStaff):
+      def __init__(self,name,department,accounting_software):
+            super().__init__(name,department)  # Call the constructor of AdminStaff
+            self.accounting_software=accounting_software
+
 t1=Teacher("Alice","Math")
 t1.change_time("4pm")  # Changing end time for Teacher
 t2=Teacher("Charlie","Science")
 a1=AdminStaff("Bob","HR")
+ac1=Accountant("David","Finance","QuickBooks")
 
 print(f"{t1.name} teaches {t1.subject} and works from {t1.start_time} to {t1.end_time}.")
 print(f"{t2.name} teaches {t2.subject} and works from {t2.start_time} to {t2.end_time}.")
