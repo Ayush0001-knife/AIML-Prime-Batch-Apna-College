0856f18db4966a8039977b7629b1d07a750d01cc
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-02 13:41:33 +0530

    Python

--- Python/practice-1.py
+++ Python/practice-1.py
@@ -11,7 +11,15 @@ class BankAccount():
             self.balance+=amount
             print("Your new balance is: ",self.balance)
 
-      def withdraw(self,amount):
-            self.balance-=amount
-            print("Your new balance is: ",self.balance)      
+      def withdraw(self, amount):
+          if amount > self.balance:
+             print("Insufficient balance!")
+          else:
+             self.balance -= amount
+             print("Your new balance is:", self.balance)      
                         
+
+person1=BankAccount(12345,"John Doe",1000)
+person1.checkBalance()
+person1.deposit(500)
+person1.withdraw(200)                        
