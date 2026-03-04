ac8f46a3a3bf1064f0a17559f2d30de020ffb125
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-04 20:00:00 +0530

    Python

--- Python/practice-7.py
+++ Python/practice-7.py
@@ -1,3 +1,10 @@
+class Person():
+      def __init__(self,name,age=None,address=None):
+            self.name=name
+            self.age=age
+            self.address=address
 
-
-            
+      def display_info(self):
+            print(f"Name: {self.name}")
+            print(f"Age: {self.age}")
+            print(f"Address: {self.address}")
